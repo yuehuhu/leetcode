c694dac470308e03496310d0caa160f57f3f30ef
yuehuhu <34811426+yuehuhu@users.noreply.github.com>
2021-06-07 13:02:07 +0800

    Create plusOne.cpp
    加一

---
+++ plusOne.cpp
@@ -0,0 +1,27 @@
+//加一
+//https://leetcode-cn.com/problems/plus-one/
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int a = digits.size();
+        vector<int> res;
+        int flag = 0;
+        int num = (digits[a - 1] + 1) % 10;
+
+        flag=(digits[a - 1] + 1) / 10;
+        res.push_back(num);
+        a--;
+
+        while(a > 0){
+            num = (digits[a - 1] + flag) % 10;
+            flag=(digits[a - 1] + flag) / 10;
+            res.push_back(num);
+            a--;
+        }
+        if(flag>0){
+            res.push_back(flag);
+        }
+        reverse(res.begin(),res.end());
+        return res;
+    }
+};
