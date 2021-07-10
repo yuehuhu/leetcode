18a4dbf1f32c5afaef0f4dc88a99f9095ad4f272
yuehuhu <34811426+yuehuhu@users.noreply.github.com>
2021-07-11 00:06:56 +0800

    Update climbStairs.cpp

--- climbStairs.cpp
+++ climbStairs.cpp
@@ -1,7 +1,7 @@
 //爬楼梯
 //https://leetcode-cn.com/problems/climbing-stairs/
 
-//解法1:动态规划 时间复杂度 O(N) 空间复杂度O(1))
+//解法1:动态规划 时间复杂度 O(N) 空间复杂度O(N)
 class Solution {
 public:
     int climbStairs(int n) {
@@ -14,5 +14,19 @@ public:
             res[i]=res[i-1]+res[i-2];
         }
         return res[n];
+    }
+};
+
+//解法2:动态规划 时间复杂度 O(N) 空间复杂度O(1)
+class Solution {
+public:
+    int climbStairs(int n) {
+        int p=1,q=1,r=1;
+        for(int i = 1; i < n; i++){
+            r=p+q;
+            p=q;
+            q=r;
+        }
+        return r;
     }
 };
