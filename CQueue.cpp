0a32af72e5ee7e7fdfce25883dfa6ad9134749fd
yuehuhu <taowhi@163.com>
2021-07-11 21:58:16 +0800

    20210711

---
+++ CQueue.cpp
@@ -0,0 +1,84 @@
+//自己的写法，时间复杂度 O(N) 空间复杂度O(1)
+//问题，其实不需要把deleteHead的stack归位
+class CQueue {
+ public:
+  stack<int> stack1, stack2;
+  CQueue() {
+    while (!stack1.empty()) {
+      stack1.pop();
+    }
+    while (!stack2.empty()) {
+      stack2.pop();
+    }
+  }
+
+  void appendTail(int value) { stack1.push(value); }
+
+  int deleteHead() {
+    if (!stack1.empty()) {
+      while (!stack1.empty()) {
+        stack2.push(stack1.top());
+        stack1.pop();
+      }
+    }
+    if (stack2.empty()) {
+      return -1;
+    }
+    int res = stack2.top();
+    stack2.pop();
+    while (!stack2.empty()) {
+      stack1.push(stack2.top());
+      stack2.pop();
+    }
+    return res;
+  }
+};
+
+/**
+ * Your CQueue object will be instantiated and called as such:
+ * CQueue* obj = new CQueue();
+ * obj->appendTail(value);
+ * int param_2 = obj->deleteHead();
+ */
+
+////////////////////////
+///////////
+//推荐写法 将stack2的数都用完以后，再存放新的数
+//时间复杂度 O(1) 空间复杂度O(1)
+class CQueue {
+ public:
+  stack<int> stack1, stack2;
+  CQueue() {
+    while (!stack1.empty()) {
+      stack1.pop();
+    }
+    while (!stack2.empty()) {
+      stack2.pop();
+    }
+  }
+
+  void appendTail(int value) { stack1.push(value); }
+
+  int deleteHead() {
+    if (stack2.empty()) {
+      while (!stack1.empty()) {
+        stack2.push(stack1.top());
+        stack1.pop();
+      }
+    }
+    if (stack2.empty()) {
+      return -1;
+    } else {
+      int res = stack2.top();
+      stack2.pop();
+      return res;
+    }
+  }
+};
+
+/**
+ * Your CQueue object will be instantiated and called as such:
+ * CQueue* obj = new CQueue();
+ * obj->appendTail(value);
+ * int param_2 = obj->deleteHead();
+ */
--- simple_algorithm/tree_questions/levelOrder.cpp
+++ simple_algorithm/tree_questions/levelOrder.cpp
@@ -1,5 +1,5 @@
 //二叉树的层序遍历
-//https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xnldjj/
+// https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xnldjj/
 
 /**
  * Definition for a binary tree node.
@@ -9,34 +9,36 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 
-//解法1:广度优先遍历 时间复杂度 O(N) 空间复杂度O(N))
+//解法1:广度优先遍历 时间复杂度 O(N) 空间复杂度O(N)
 class Solution {
-public:
-    vector<vector<int>> levelOrder(TreeNode* root) {
-        queue<TreeNode*> Q;
-        vector<vector<int>> res;
-        if(!root) return res;
-        Q.push(root);
-        while(!Q.empty()){
-            int size=Q.size();
-            vector<int> tmp;
-            while(size>0){
-                TreeNode* node1=Q.front();Q.pop();
-                tmp.push_back(node1->val);
-                if(node1->left){
-                    Q.push(node1->left);
-                }
-                if(node1->right){
-                    Q.push(node1->right);
-                }
-                size--;
-            }
-            res.push_back(tmp);
+ public:
+  vector<vector<int>> levelOrder(TreeNode* root) {
+    queue<TreeNode*> Q;
+    vector<vector<int>> res;
+    if (!root) return res;
+    Q.push(root);
+    while (!Q.empty()) {
+      int size = Q.size();
+      vector<int> tmp;
+      while (size > 0) {
+        TreeNode* node1 = Q.front();
+        Q.pop();
+        tmp.push_back(node1->val);
+        if (node1->left) {
+          Q.push(node1->left);
         }
-        return res;
+        if (node1->right) {
+          Q.push(node1->right);
+        }
+        size--;
+      }
+      res.push_back(tmp);
     }
+    return res;
+  }
 };
